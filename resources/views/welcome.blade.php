bb3d9044e90311ddb0464145364bd2e5ef160486
Gigamick <michael@songbox.rocks>
2024-08-11 16:22:01 +0100

    mobile optimised

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,18 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-12 d-flex justify-content-end">
+                <!-- Share to Twitter Button -->
+                <button id="share-to-twitter" style="border:none; background: transparent" class="twitter-share-button">
+                    <span style="font-size: 26px;">𝕩</span>
+                </button>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-12 d-flex justify-content-center flex-column align-items-center">
                 <h1 class="mb-0 pb-0 mt-5 display-1" style="font-weight: 900;">Nickelback</h1>
                 <h3 class="mt-0 pt-0 display-3" style="font-weight: 200; line-height: 50px"><i>IPSUM</i></h3>
-                <p class="mt-4 tagline text-center">The most Rockin' Ipsum generator on the internet.</p>
+                <p class="mt-4 tagline text-center">This is how you remind me what my design looks like with copy.</p>
             </div>
         </div>
         <div class="row">
@@ -111,6 +119,19 @@
                                 document.getElementById('ipsumOutput').innerText = 'An error occurred: ' + error.message;
                             });
                     });
+
+                    document.getElementById('share-to-twitter').addEventListener('click', function() {
+                        // Customize the text and URL to be shared
+                        var text = "Nickelback Ipsum! - This is how you remind me what my design looks like with copy. #nickelback #ipsum #loremipsum #webdev #webdesign #webdevelopment  #webdesigner #webdesigners #webdevelopers #webdevs #webdevelopment https://nickelback-ipsum.com";
+                        var url = "https://nickelback-ipsum.com?ref=twitter"; // Replace with your actual URL
+
+                        // Construct the Twitter share URL
+                        var twitterUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text) + "&url=" + encodeURIComponent(url);
+
+                        // Open the Twitter share dialog in a new window
+                        window.open(twitterUrl, "_blank");
+                    });
+
                 </script>
 
 
