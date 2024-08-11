66c008c26a54c0fb77dc865dc7e8b55fc0707420
Gigamick <michael@songbox.rocks>
2024-08-11 16:23:21 +0100

    mobile optimised

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12 d-flex justify-content-end">
+            <div class="col-12 col-md-6 offset-md-3 d-flex justify-content-end">
                 <!-- Share to Twitter Button -->
                 <button id="share-to-twitter" style="border:none; background: transparent" class="twitter-share-button">
                     <span style="font-size: 26px;">𝕩</span>
@@ -12,7 +12,7 @@
         </div>
         <div class="row">
             <div class="col-12 d-flex justify-content-center flex-column align-items-center">
-                <h1 class="mb-0 pb-0 mt-5 display-1" style="font-weight: 900;">Nickelback</h1>
+                <h1 class="mb-0 pb-0 mt-4 display-1" style="font-weight: 900;">Nickelback</h1>
                 <h3 class="mt-0 pt-0 display-3" style="font-weight: 200; line-height: 50px"><i>IPSUM</i></h3>
                 <p class="mt-4 tagline text-center">This is how you remind me what my design looks like with copy.</p>
             </div>
