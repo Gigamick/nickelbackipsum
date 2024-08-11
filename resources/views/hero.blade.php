4ab00da36c8911a3c2522021f87670ebf1ce0dbd
Gigamick <michael@songbox.rocks>
2024-08-11 23:51:11 +0100

    about page

--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -16,6 +16,6 @@
     <div class="col-12 d-flex justify-content-center flex-column align-items-center">
         <h1 class="mb-0 pb-0 mt-4 display-1" style="font-weight: 900;">Nickelback</h1>
         <h3 class="mt-0 pt-0 display-3" style="font-weight: 200; line-height: 50px"><i>IPSUM</i></h3>
-        <p class="mt-4 tagline text-center">This is how you remind me what my design looks like with copy.</p>
+        <p class="mt-4 tagline text-center">This is how you remind me of what my design looks like with copy.</p>
     </div>
 </div>
